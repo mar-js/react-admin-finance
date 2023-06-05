723deafbe3986effa5263bd81f1611ed51a4f035
Martin <maguera37@gmail.com>
2023-06-05 19:51:07 -0300

    feat: added type in reducer of transactions

--- src/store/reducers/transactions/index.tsx
+++ src/store/reducers/transactions/index.tsx
@@ -1,5 +1,7 @@
+import { ADD_TRANSACTION } from 'store/types'
+
 export const transactionsReducer = (state, action) => {
-  if (state === 'ADD_TRANSACTION') return state
+  if (state === ADD_TRANSACTION) return state
 
   return state
 }
