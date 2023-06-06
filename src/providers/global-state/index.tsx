fc4c5ec4fd752e23d70ac470c0d7786ed213d083
Martin <maguera37@gmail.com>
2023-06-06 19:51:07 -0300

    feat: added useReducer

--- src/providers/global-state/index.tsx
+++ src/providers/global-state/index.tsx
@@ -1,9 +1,14 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useReducer } from 'react'
 import { GlobalStateModel } from 'contexts'
+import { transactionsReducer } from 'store/reducers'
+import { INITIAL_STATE } from 'constants'
 
 export const GlobalStateProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [state, dispatch] = useReducer(transactionsReducer, INITIAL_STATE)
 
-  <GlobalStateModel.Provider value={ null }>
+  const VALUE = { state }
+
+  <GlobalStateModel.Provider value={ VALUE }>
     { children }
   </GlobalStateModel.Provider>
 }
