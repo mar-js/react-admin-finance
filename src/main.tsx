38e530a3e455024b96683eefc4a1142d106ad055
Martin <maguera37@gmail.com>
2023-06-08 19:51:07 -0300

    feat: added GlobalStateProvider component in the proyect

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GlobalStateProvider } from 'providers'
 import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalStateProvider>
+      <App />
+    </GlobalStateProvider>
   </React.StrictMode>
 )
