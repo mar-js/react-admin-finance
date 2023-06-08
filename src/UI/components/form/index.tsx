9864efef60ca8b64c30f5a7934c1bd596ea4d009
Martin <maguera37@gmail.com>
2023-06-08 19:51:07 -0300

    feat: added handleSubmit to onSubmit of form

--- src/UI/components/form/index.tsx
+++ src/UI/components/form/index.tsx
@@ -1,7 +1,29 @@
+import { FormEvent } from 'react'
+import { useGlobalState } from 'contexts'
+import { ADD_TRANSACTION } from 'store/types'
+import { ITransaction } from 'interfaces'
+
 export const Form: React.FC = () => {
+  const { dispatch } = useGlobalState()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    const TARGET = e.target as HTMLFormElement
+    const DATA = Object.fromEntries(new FormData(TARGET))
+    const { description, amount } = DATA as unknown as ITransaction
+
+    dispatch({
+      type: ADD_TRANSACTION,
+      payload: {
+        description,
+        amount
+      }
+    })
+  }
 
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <input type="text" name="description" />
       <input type="number" step="0.1" name="amount" />
       <button type="submit">Add transaction</button>
