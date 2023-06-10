2093f2a10a7ec1c0ef7122512ae3b1d02b2f78a5
Martin <maguera37@gmail.com>
2023-06-10 19:51:07 -0300

    feat: created deleteTransaction in provider

--- src/providers/global-state/index.tsx
+++ src/providers/global-state/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, useReducer } from 'react'
 import { GlobalStateModel } from 'contexts'
 import { transactionsReducer } from 'store/reducers'
 import { INITIAL_STATE } from 'constants'
-import { ADD_TRANSACTION } from 'store/types'
+import { ADD_TRANSACTION, DELETE_TRANSACTION } from 'store/types'
 import { ITransaction } from 'interfaces'
 
 export const GlobalStateProvider: React.FC<PropsWithChildren> = ({ children }) => {
@@ -15,10 +15,18 @@ export const GlobalStateProvider: React.FC<PropsWithChildren> = ({ children }) =
     })
   }
 
+  const deleteTransaction = (transaction: ITransaction) => {
+    dispatch({
+      type: DELETE_TRANSACTION,
+      payload: transaction
+    })
+  }
+
   const VALUE = {
     transactions,
     dispatch,
-    addTransaction
+    addTransaction,
+    deleteTransaction
   }
 
   return (
