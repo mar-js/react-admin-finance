fadcc4a15d913a621bf145a92cb673b59fe73c13
Martin <maguera37@gmail.com>
2023-06-11 19:51:07 -0300

    feat: added deleteTransaction and created button for delete transaction

--- src/UI/components/transactions/index.tsx
+++ src/UI/components/transactions/index.tsx
@@ -1,13 +1,14 @@
 import { useGlobalState } from 'contexts'
 
 export const Transactions: React.FC = () => {
-  const { transactions } = useGlobalState()
+  const { transactions, deleteTransaction } = useGlobalState()
 
   return (
     <ul>
       { transactions.map(transaction => (
         <li key={ transaction.id }>
           { transaction.description } - { transaction.amount }
+          <button type="button" onClick={ () => deleteTransaction(transaction) }>X</button>
         </li>
       )) }
     </ul>
