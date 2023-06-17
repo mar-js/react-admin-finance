24d0beae059af25d698728c03ac74389f86e98fb
Martin <maguera37@gmail.com>
2023-06-17 19:51:07 -0300

    feat: correction return of local-storage-data

--- src/helpers/local-storage-data/index.ts
+++ src/helpers/local-storage-data/index.ts
@@ -3,5 +3,7 @@ import { INITIAL_STATE } from 'constants'
 export const localStorageData = () => {
   const LOCAL_DATA = localStorage.getItem('transactions')
 
-  return LOCAL_DATA ? JSON.parse(LOCAL_DATA) : INITIAL_STATE
+  const TRANSACTIONS = LOCAL_DATA ? JSON.parse(LOCAL_DATA) : INITIAL_STATE
+
+  return { transactions: TRANSACTIONS }
 }
