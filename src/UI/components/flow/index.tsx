4f961ebc9715506011d8fcfe56575b6855223bee
Martin <maguera37@gmail.com>
2023-06-18 19:51:07 -0300

    feat: added styles in flow component

--- src/UI/components/flow/index.tsx
+++ src/UI/components/flow/index.tsx
@@ -4,9 +4,9 @@ export const Flow: React.FC = () => {
   const { INCOME, EXPENSE } = useGlobalState()
 
   return (
-    <div>
-      <h4>Income: { INCOME }</h4>
-      <h4>Expense: { EXPENSE }</h4>
+    <div className="text-md">
+      <h4 className="text-inherit mb-2">Income: { INCOME }</h4>
+      <h4 className="text-inherit mb-2">Expense: { EXPENSE }</h4>
     </div>
   )
 }
