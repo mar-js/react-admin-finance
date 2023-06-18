53ca9a5453e136dc0bfa5ac5c26fc64e075b0272
Martin <maguera37@gmail.com>
2023-06-18 19:51:07 -0300

    feat: added style in balance component

--- src/UI/components/balance/index.tsx
+++ src/UI/components/balance/index.tsx
@@ -4,6 +4,6 @@ export const Balance: React.FC = () => {
   const { TOTAL } = useGlobalState()
 
   return (
-    <h2>{ TOTAL }</h2>
+    <h2 className="text-xl">Your balance: <strong className="text-2xl">{ TOTAL }</strong></h2>
   )
 }
