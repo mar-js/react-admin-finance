bae6827e6c07d7f3f25aa6edb82d83556facbaf7
Martin <maguera37@gmail.com>
2023-06-22 19:51:07 -0300

    feat: added styles to the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GlobalStateProvider } from 'providers'
 import { App } from './App.tsx'
+import 'styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
