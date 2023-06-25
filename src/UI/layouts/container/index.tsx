7f9a11b207b0a1fd1dc61fd3313f5977d17c049c
Martin <maguera37@gmail.com>
2023-06-24 21:36:31 -0300

    feat: removed h-full of container

--- src/UI/layouts/container/index.tsx
+++ src/UI/layouts/container/index.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export const Container: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="h-full w-10/12 m-auto">
+  <div className="w-10/12 m-auto">
     { children }
   </div>
 )
