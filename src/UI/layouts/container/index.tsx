04af747d78c7bb4d606bea57fa83eabfc74b2f26
Martin <maguera37@gmail.com>
2023-06-25 19:51:07 -0300

    feat: added styling to container layout

--- src/UI/layouts/container/index.tsx
+++ src/UI/layouts/container/index.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export const Container: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="w-10/12 m-auto flex justify-between items-center">
+  <div className="w-9/12 m-auto flex flex-col justify-center items-center">
     { children }
   </div>
 )
