b665371f267d656ef6c01261fa140d110b395d97
Martin <maguera37@gmail.com>
2023-06-26 19:51:07 -0300

    feat: added styling to transactions component

--- src/UI/components/transactions/index.tsx
+++ src/UI/components/transactions/index.tsx
@@ -4,13 +4,24 @@ export const Transactions: React.FC = () => {
   const { transactions, deleteTransaction } = useGlobalState()
 
   return (
-    <ul>
-      { transactions.map(transaction => (
-        <li key={ transaction.id }>
-          { transaction.description } | { transaction.amount }
-          <button type="button" onClick={ () => deleteTransaction(transaction) }>X</button>
-        </li>
-      )) }
-    </ul>
+    <>
+      { transactions.length > 0 && (
+        <div className="py-3 my-6 border-t border-t-gray-400">
+          <h4 className="text-xl">History</h4>
+          <ul>
+            { transactions.map(transaction => (
+              <li key={ transaction.id } className="flex justify-between items-center bg-lime-700 text-white p-3 mb-2 text-xs">
+                { transaction.description } ${ transaction.amount }
+                <button
+                  className="font-bold border rounded-full py-2 px-3 bg-white text-black"
+                  type="button"
+                  onClick={ () => deleteTransaction(transaction) }
+                >X</button>
+              </li>
+            )) }
+          </ul>
+        </div>
+      ) }
+    </>
   )
 }
