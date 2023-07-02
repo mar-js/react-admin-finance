48ed1a7a9ff8b961dc3e59c955df6e1c08beb9f8
Martin <maguera37@gmail.com>
2023-07-02 19:51:07 -0300

    feat: added styling to navigation component

--- src/UI/layouts/navigation/index.tsx
+++ src/UI/layouts/navigation/index.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export const Navigation: React.FC<PropsWithChildren> = ({ children }) => (
-  <nav>
+  <nav className="w-full">
     { children }
   </nav>
 )
