f1307c7a0485559a693771b9f661351573a966ac
Martin <maguera37@gmail.com>
2023-07-04 19:51:07 -0300

    feat: added width to ExpenseChart component

--- src/UI/components/expense-chart/index.tsx
+++ src/UI/components/expense-chart/index.tsx
@@ -1,25 +1,27 @@
 import { VictoryPie, VictoryLabel } from 'victory'
 
 export const ExpenseChart: React.FC = () => (
-  <VictoryPie
-    colorScale={ [ 'red', 'blue' ] }
-    data={ [
-      {
-        x: 'Expenses',
-        y: 35
-      },
-      {
-        x: 'Incomes',
-        y: 40
-      }
-    ] }
-    animate={ { duration: 300 } }
-    labels={ ({ datum }) => `${datum.y}%` }
-    labelComponent={ (
-      <VictoryLabel
-        angle={ 45 }
-        style={ { fill: 'white' } }
-      />
-    ) }
-  />
+  <div className="w-[500px]">
+    <VictoryPie
+      colorScale={ [ 'red', 'blue' ] }
+      data={ [
+        {
+          x: 'Expenses',
+          y: 35
+        },
+        {
+          x: 'Incomes',
+          y: 40
+        }
+      ] }
+      animate={ { duration: 300 } }
+      labels={ ({ datum }) => `${datum.y}%` }
+      labelComponent={ (
+        <VictoryLabel
+          angle={ 45 }
+          style={ { fill: 'white' } }
+        />
+      ) }
+    />
+  </div>
 )
