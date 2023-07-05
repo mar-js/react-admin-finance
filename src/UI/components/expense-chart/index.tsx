65886dbb256d08de82d9bc6fcc41c3634ef113b4
Martin <maguera37@gmail.com>
2023-07-05 19:51:07 -0300

    feat: added total of expense and income percentage in victoryPie

--- src/UI/components/expense-chart/index.tsx
+++ src/UI/components/expense-chart/index.tsx
@@ -1,27 +1,32 @@
+import { useGlobalState } from 'contexts'
 import { VictoryPie, VictoryLabel } from 'victory'
 
-export const ExpenseChart: React.FC = () => (
-  <div className="w-[500px]">
-    <VictoryPie
-      colorScale={ [ 'red', 'blue' ] }
-      data={ [
-        {
-          x: 'Expenses',
-          y: 35
-        },
-        {
-          x: 'Incomes',
-          y: 40
-        }
-      ] }
-      animate={ { duration: 300 } }
-      labels={ ({ datum }) => `${datum.y}%` }
-      labelComponent={ (
-        <VictoryLabel
-          angle={ 45 }
-          style={ { fill: 'white' } }
-        />
-      ) }
-    />
-  </div>
-)
+export const ExpenseChart: React.FC = () => {
+  const { TOTAL_EXPENSE_PERCENTAGE, TOTAL_INCOME_PERCENTAGE } = useGlobalState()
+
+  return (
+    <div className="w-[500px]">
+      <VictoryPie
+        colorScale={ [ 'red', 'blue' ] }
+        data={ [
+          {
+            x: 'Expenses',
+            y: TOTAL_EXPENSE_PERCENTAGE
+          },
+          {
+            x: 'Incomes',
+            y: TOTAL_INCOME_PERCENTAGE
+          }
+        ] }
+        animate={ { duration: 300 } }
+        labels={ ({ datum }) => `${datum.y}%` }
+        labelComponent={ (
+          <VictoryLabel
+            angle={ 45 }
+            style={ { fill: 'white' } }
+          />
+        ) }
+      />
+    </div>
+  )
+}
