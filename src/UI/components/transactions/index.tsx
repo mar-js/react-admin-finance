29045ecefc7dc15e7c45f02d4b0df1e8fcccfbfd
Martin <maguera37@gmail.com>
2023-07-07 21:37:41 -0300

    feat: changed style of transactions component

--- src/UI/components/transactions/index.tsx
+++ src/UI/components/transactions/index.tsx
@@ -6,14 +6,14 @@ export const Transactions: React.FC = () => {
   return (
     <>
       { transactions.length > 0 && (
-        <div className="py-3 my-6 border-t border-t-gray-400">
-          <h4 className="text-xl">History</h4>
+        <div className="p-3 border-t border-t-gray-400 h-full bg-green-700">
+          <h4 className="text-xl text-white mb-4">History</h4>
           <ul>
             { transactions.map(transaction => (
               <li key={ transaction.id } className="flex justify-between items-center bg-lime-700 text-white p-3 mb-2 text-xs">
                 { transaction.description } ${ transaction.amount }
                 <button
-                  className="font-bold border rounded-full py-2 px-3 bg-white text-black"
+                  className="font-bold border rounded-full py-1 px-2 bg-white text-black text-xs ml-4"
                   type="button"
                   onClick={ () => deleteTransaction(transaction) }
                 >X</button>
