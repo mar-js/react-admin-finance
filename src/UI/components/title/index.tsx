b3182d64ac1c9e847fd499492ad3a5f2c327d163
Martin <maguera37@gmail.com>
2023-07-07 21:37:12 -0300

    feat: changed style of title component

--- src/UI/components/title/index.tsx
+++ src/UI/components/title/index.tsx
@@ -1,3 +1,3 @@
 export const Title: React.FC = () => (
-  <h1 className="text-lime-950 text-center text-xl">Expense Tracker</h1>
+  <h1 className="text-lime-800 mb-3 text-center text-3xl font-bold">Expense Tracker</h1>
 )
